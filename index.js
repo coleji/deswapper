e4fa4b95cfdd9e00e7b7bbb5ae847a8e63fc97da
Jonathan Cole <jcole@hobie>
2019-05-28 10:19:06 -0400

    dev and tested swap toggle if enough room

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const exec = require("child_process").exec
 const getFreeMem = require("./getFreeMem")
 const getSwap = require("./getSwap")
 
@@ -5,6 +6,11 @@ getFreeMem()
 .then(free => {
 	return getSwap().then(swap => {
 		console.log(`swap using ${swap}, free mem is ${free}`)
+		const atLeastTwiceEnoughSpace = free >= swap * 2;
+		console.log("at least twice enough space?  " + atLeastTwiceEnoughSpace);
+		if (atLeastTwiceEnoughSpace) {
+			exec("sudo swapoff -a && sudo swapon -a")
+		}
 	})
 }).catch(err => {
 	console.log(err)
