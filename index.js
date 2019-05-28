55a9a1b458bc31d8e944ded633b4c9df1369d1d9
Jonathan <coleji@bc.edu>
2019-05-28 10:27:14 -0400

    logging, bail on 0 swap use

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
+var moment = require('moment')
 const exec = require("child_process").exec
 const getFreeMem = require("./getFreeMem")
 const getSwap = require("./getSwap")
 
+
+const now = moment().format("YYYY-MM-DD HH:mm:ss")
+console.log("==================")
+console.log(now)
+console.log("starting deswapper")
 getFreeMem()
 .then(free => {
-	return getSwap().then(swap => {
+	return getSwap().then(swap => new Promise((resolve, reject) => {
 		console.log(`swap using ${swap}, free mem is ${free}`)
-		const atLeastTwiceEnoughSpace = free >= swap * 2;
-		console.log("at least twice enough space?  " + atLeastTwiceEnoughSpace);
-		if (atLeastTwiceEnoughSpace) {
-			exec("sudo swapoff -a && sudo swapon -a")
+		if (swap == 0) {
+			resolve("Nothing to deswap")
+			return;
 		}
-	})
-}).catch(err => {
-	console.log(err)
-})
+		const enoughSpaceForDeswap = (free >= (swap * 2) + 100000);
+		if (enoughSpaceForDeswap) {
+			exec("sudo swapoff -a && sudo swapon -a", (err, stdout, stderr) => {
+				if (err) reject(err)
+				else resolve("Successfully deswapped")
+			})
+		} else reject("Not enough free for deswap")
+	}))
+}).then(
+	success => {
+		console.log("Successful run: " + success)
+	},
+	err => {
+		console.log("Deswap error!   " + err)
+	}
+)
